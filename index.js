f4551264732ca5c259e00f4c70d5b7e54190d5e0
hauroque <luizaugusto_sv@hotmail.com>
2024-08-16 16:30:57 -0400

    Identation

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 //Body Parser
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.get("/", (req, res) => {
--- seeds/001_users.js
+++ seeds/001_users.js
@@ -2,11 +2,11 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> } 
  */
-exports.seed = async function(knex) {
+exports.seed = async function (knex) {
   // Deletes ALL existing entries
   await knex('users').del()
   await knex('users').insert([
-    {username: 'Luiz Augusto'},
-    {username: 'Isabella Veloso'}
+    { username: 'Luiz Augusto' },
+    { username: 'Isabella Veloso' }
   ]);
 };
