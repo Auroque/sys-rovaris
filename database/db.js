9aaa5c741082cff61bc348e40068fc0e3b82f97a
hauroque <luizaugusto_sv@hotmail.com>
2024-08-20 14:22:18 -0400

    Smart

--- database/db.js
+++ database/db.js
@@ -5,4 +5,4 @@ const knexConfig = require('../knexfile');
 const environment = process.env.NODE_ENV || 'development';
 const db = knex(knexConfig[environment]);
 
-module.exports = db;
+module.exports = db
